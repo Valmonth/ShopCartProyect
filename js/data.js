11bce11e0f92eff60b83fd3d244e015e548b2e5a
valmonth <valmonth15@gmail.com>
2022-06-10 19:37:48 -0600

    Update

---
+++ js/data.js
@@ -0,0 +1,15 @@
+let productos = [
+    {name: 'Camisa',
+     image: './img/camisa.png',
+     price: 200},
+     {name: 'Pantalon',
+     image: './img/pantalon.png',
+     price: 150},
+     {name: 'Tenis',
+     image: './img/tenis.png',
+     price: 60},
+     {name: 'Medias',
+     image: './img/medias.png',
+     price: 10},
+
+]
